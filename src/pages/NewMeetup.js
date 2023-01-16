c08775007f004f4bc07113f9477e97e63dfe03bd
Burim Palloshi <burim-palloshi@hotmail.com>
2023-01-16 19:15:42 +0100

    update

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,26 @@
+import { useNavigate } from "react-router-dom";
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+  const navigate = useNavigate();
+
+  function addMeetupHandler(meetupData) {
+    fetch("https://react-meetup-1-default-rtdb.firebaseio.com/meetups.json", {
+      method: "POST",
+      body: JSON.stringify(meetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then(() => {
+      navigate("/", { replace: true });
+    });
+  }
+
   return (
     <section>
       <h1>New Meetup Page</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
